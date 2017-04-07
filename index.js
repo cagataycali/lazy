d239e0eda4a9d26157d54a4cf442a879e914193e
cagataycali <cagataycali@icloud.com>
2017-04-07 07:32:35 +0300

    ✔️  Added

--- index.js
+++ index.js
@@ -1,150 +1,151 @@
-var BrainJSClassifier = require('natural-brain')
-var classifier = new BrainJSClassifier();
-
-var TelegramBot = require('node-telegram-bot-api');
-var appUrl = `https://${process.env.APP_NAME}.herokuapp.com:443`;
-var options = { webHook: { port: process.env.PORT } };
-var bot = new TelegramBot(process.env.TELEGRAM_TOKEN, options);
-bot.setWebHook(`${appUrl}/bot${process.env.TELEGRAM_TOKEN}`);
-
-var  _ = require('underscore')
-
+var BrainJSClassifier = require('natural-brain');
 var Datastore = require('nedb');
+var  _ = require('underscore')
 var categories = new Datastore({ filename: 'categories.db', autoload: true });
-var nonclassified = new Datastore({ filename: 'nonclassified.db', autoload: true });
 
 categories.ensureIndex({ fieldName: 'name', unique: true }, function (err) {
   if (err) console.log(err);
 });
 
-nonclassified.ensureIndex({ fieldName: 'word', unique: true }, function (err) {
-  if (err) console.log(err);
-});
+function random(items) {return items[Math.floor(Math.random()*items.length)]};
 
-var slient = false;
-
-const empty = () => {};
-
-const random = (items) => {return items[Math.floor(Math.random()*items.length)]};
-
-function matcher(matchs, pattern, message, callback, error) {
-  if (message.text.includes(matchs)) {
-    callback(message.text.match(pattern));
-  } else {
-    error();
+class Lazy {
+  constructor() {
+    this.classifier = new BrainJSClassifier();
+    this.categories = categories;
+    this.slient = false;
   }
-}
 
-function check(text, callback) {
-    var classified = classifier.getClassifications(text);
+  learn(obj) {
+    let {classifier, categories} = this;
+    return new Promise(function(resolve, reject) {
+      classifier.addDocument(obj.phrase, obj.category);
+      classifier.retrain();
+      categories.insert({name:obj.category, responses: []}, function(err, doc) {
+        resolve(doc)
+      });
+    });
+  }
+
+  getCategories() {
+    let categories = this.categories;
+    return new Promise(function(resolve, reject) {
+      categories.find({}, function (err, docs) {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(docs);
+          }
+      });
+    });
+  }
+
+  quiet() {
+    let {slient, categories} = this;
+    return new Promise(function(resolve, reject) {
+      slient = slient === true ? false : true;
+      resolve(slient);
+    });
+  }
+
+  getResponses(obj) {
+    let categories = this.categories;
+    return new Promise(function(resolve, reject) {
+      categories.findOne({name: obj.category}, function (err, docs) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(docs.responses)
+        }
+      });
+    });
+  }
+
+  addResponse(obj) {
+    let categories = this.categories;
+    return new Promise(function(resolve, reject) {
+      categories.update({ name: obj.category }, { $push: { responses: obj.response } }, {}, function (err) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+
+  query(obj) {
+    console.log(obj);
+    let {categories, slient, classifier} = this;
+    let classified = classifier.getClassifications(obj.phrase);
     classified = _.sortBy(classified, 'value')
     classified.reverse();
-    var value = classified[0].value;
+    let possibility = 0;
+    let value = classified[0].value;
     if (value > 0.8) {
-      callback(1);
+      possibility = 1;
     } else if (value > 0.3 && value < 0.8) {
-      callback(0.5);
+      possibility = 0.5;
     } else {
-      callback(0);
+      possibility = 0;
     }
+    const category = classifier.classify(obj.phrase)
+
+    return new Promise(function(resolve, reject) {
+      categories.findOne({name: category}, function (err, docs) {
+          if (!slient) {
+            var response = random(docs.responses);
+            resolve({status: true, possibility, response, details: classified})
+          }
+          classifier.addDocument(obj.phrase, category); // Add document
+          classifier.retrain(); // Re train! :)
+      });
+    });
   }
 
-bot.on('message', (msg) => {
-
-  if (msg.entities && msg.entities[0].type === "bot_command") {
-
-    matcher('/learn', /\/learn (.+) - (.+)/, msg, function(match) {
-      try {
-        classifier.addDocument(match[1], match[2]);
-        classifier.retrain();
-        categories.insert({name:match[2], responses: []}, function(err, doc) {
-          if (!err) console.log('Inserted', doc.name, 'with ID', doc._id)
-        });
-        bot.sendMessage(msg.chat.id, 'Hmm.. 😕', { disable_notification:true, reply_to_message_id:msg.message_id });
-      } catch (e) {
-        bot.sendMessage(msg.chat.id, 'I forgot already.. 😕', { disable_notification:true, reply_to_message_id:msg.message_id });
-      }
-    }, empty);
-
-    matcher('/load', /\/load/, msg, function(match) {
+  loadTrainedData() {
+    try {
+      var classifier = this.classifier;
       BrainJSClassifier.load('./data.json', null,
         function(err, newClassifier) {
           classifier = newClassifier;
           classifier.train();
-        });
-      }, empty);
-
-    matcher('/save', /\/save/, msg, function(match) {
-        classifier.save('./data.json', function() {
-          console.log('Saved..');
-        });
-    }, empty);
-    matcher('/forgot', /\/forgot (.+) - (.+)/, msg, function(match) {
-      classifier.removeDocument(match[1], match[2]);
-      classifier.retrain();
-      classifier.remove({ name:match[2] }, {}, function (err, numRemoved) {
-        console.log(numRemoved, 'row removed.');
-      });
-      bot.sendMessage(msg.chat.id, 'I forgot already.. 😕', { disable_notification:true, reply_to_message_id:msg.message_id });
-    }, empty);
-
-    matcher('/categories', /\/categories/, msg, function(match) {
-      categories.find({}, function (err, docs) {
-          bot.sendMessage(msg.chat.id, `${docs.map((item) => {return item.name}).join(',')}`, { disable_notification:true, reply_to_message_id:msg.message_id });
-      });
-    }, empty);
-
-    matcher('/quiet', /\/quiet/, msg, function(match) {
-      slient = slient === true ? false : true;
-    }, empty);
-
-    matcher('/responses', /\/responses (.+)/, msg, function(match) {
-      try {
-        categories.findOne({name: match[1]}, function (err, docs) {
-          console.log(docs.responses);
-          bot.sendMessage(msg.chat.id, `${docs.responses.join(', ')}`, { disable_notification:true, reply_to_message_id:msg.message_id });
-        });
-      } catch (e) {
-        bot.sendMessage(msg.chat.id, `Pwiz check usage :(`, { disable_notification:true, reply_to_message_id:msg.message_id });
-      }
-
-    }, empty);
-
-    matcher('/add', /\/add (.+) - (.+)/, msg, function (match) {
-      try {
-        categories.update({ name: match[1] }, { $push: { responses: match[2] } }, {}, function (err) {
-          bot.sendMessage(msg.chat.id, 'I think I can! .. 😕', { disable_notification:true, reply_to_message_id:msg.message_id });
-        });
-      } catch (e) {
-        bot.sendMessage(msg.chat.id, `I can't understand well, anybody can explain .. 😕`, { disable_notification:true, reply_to_message_id:msg.message_id });
-      }
-    }, empty);
-
-  } else {
-    try {
-      var category = classifier.classify(msg.text);
-      console.log(msg.text, category, classifier.getClassifications(msg.text));
-      check(msg.text, function(status) {
-        if (status === 1) {
-          categories.findOne({name: category}, function (err, docs) {
-              if (!slient) {
-                bot.sendMessage(msg.chat.id, `${random(docs.responses)}`, { disable_notification:true, reply_to_message_id:msg.message_id });
-              }
-              classifier.addDocument(msg.text, category); // Add document
-              classifier.retrain(); // Re train! :)
-          });
-        } else if (status === 0.5) {
-          if (!slient) {
-            bot.sendMessage(msg.chat.id, `I can't understand well, anybody can explain?`, { disable_notification:true, reply_to_message_id:msg.message_id });
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('Done..');
           }
-        }
-      })
-
+        });
+        this.classifier = classifier;
     } catch (e) {
-      console.log('non classified',msg.text);
-      nonclassified.insert({word: msg.text}, function(err, doc) {
-        if (!err) console.log('Inserted', doc.word, 'with ID', doc._id)
-      });
+      console.log('Error when loading.');
     }
   }
-});
+
+  save() {
+    var classifier = this.classifier;
+    return new Promise(function(resolve, reject) {
+      classifier.save('./data.json', function() {
+        resolve(true);
+      });
+    });
+  }
+
+  removeDocument(obj) {
+    var classifier = this.classifier;
+    return new Promise(function(resolve, reject) {
+      classifier.removeDocument(obj.phrase, obj.category);
+      classifier.retrain();
+      classifier.remove({ name:obj.category }, {}, function (err, numRemoved) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+}
+
+
+module.exports = Lazy;
